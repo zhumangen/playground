615f4e803f6028aad6870f6a0c8d463069e0a827
Mangen.Zhu <zhumangen@163.com>
2018-12-05 19:05:26 +0800

    add dcm dependencies

---
+++ src/config.ts
@@ -0,0 +1,39 @@
+import * as cornerstone from 'cornerstone-core';
+import * as cornerstoneTools from 'cornerstone-tools';
+import * as cornerstoneMath from 'cornerstone-math';
+import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+import * as dicomParser from 'dicom-parser';
+import Hammer from 'hammerjs';
+
+cornerstoneTools.external.Hammer = Hammer;
+cornerstoneTools.external.cornerstone = cornerstone;
+cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
+cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
+
+const maxWebWorkers = Math.max(navigator.hardwareConcurrency - 1, 1);
+const config = {
+  maxWebWorkers: maxWebWorkers,
+	startWebWorkersOnDemand: true,
+    webWorkerPath : 'lib/cornerstoneWADOImageLoaderWebWorker.js',
+    taskConfiguration: {
+        'decodeTask' : {
+        loadCodecsOnStartup : true,
+        initializeCodecsOnStartup: false,
+            codecsPath: 'lib/cornerstoneWADOImageLoaderCodecs.js',
+            usePDFJS: false
+        }
+    }
+};
+
+cornerstoneWADOImageLoader.webWorkerManager.initialize(config);
+
+// Export scripts that will populate the Cornerstone namespace as a side effect only import.
+// This is effectively the public API...
+export {
+    cornerstone,
+    cornerstoneTools,
+    cornerstoneMath,
+    cornerstoneWADOImageLoader,
+    dicomParser
+};
